e50dc3500bd3bea13c9de1d720b17e558a9320bf
Samitha Hewawasam <samitha@techleadintl.com>
2017-12-16 02:13:47 +0530

    build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 git stash -u
 git checkout master
 git pull origin master
-docker build --force -rm -t node1:5000/ebuy-frontend-web .
+docker build --force-rm -t node1:5000/ebuy-frontend-web .
 docker push node1:5000/ebuy-frontend-web
